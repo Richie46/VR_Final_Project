91543947ba0f90a76ee3f3a1b5a3607ca8846ac2
Dawson <djtorch123@gmail.com>
2020-12-19 17:14:51 -0500

    Added Footsteps and Menu

---
+++ FinalProject_RDell_DTocarchick/Assets/Footsteps.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    CharacterController cc;
+
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cc.isGrounded == true && cc.velocity.magnitude > 2f && GetComponent<AudioSource>().isPlaying == false)
+        {
+            GetComponent<AudioSource>().volume = Random.Range(0.8f, 1);
+            GetComponent<AudioSource>().pitch = Random.Range(0.8f, 1.1f);
+            GetComponent<AudioSource>().panStereo = Random.Range(-0.8f, 0.8f);
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
