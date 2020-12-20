69b764991da974e5f54ac7945fade1ec6444dcf7
Dawson <djtorch123@gmail.com>
2020-12-20 05:43:34 -0500

    Gameplay added

--- FinalProject_RDell_DTocarchick/Assets/Scripts/TriggerScript.cs
+++ FinalProject_RDell_DTocarchick/Assets/Scripts/TriggerScript.cs
@@ -1,11 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class TriggerScript : MonoBehaviour
 {
-   void OnTriggerEnter (Collider other)
+    public GameObject paper;
+    public GameObject paper2;
+    public Material material1;
+    public GameObject chooseObject;
+    public GameObject thisTask;
+    public GameObject Task2;
+
+    public GameObject soundObject;
+
+
+    void Start()
     {
-        Debug.Log("TRIGGGG");
+        Task2.SetActive(false);
+
+    }
+   void OnTriggerEnter (Collider col)
+    {
+        if (col.gameObject == chooseObject)
+        {
+            paper.GetComponent<MeshRenderer>().material = material1;
+            paper2.GetComponent<MeshRenderer>().material = material1;
+            Debug.Log("Task Completed");
+            thisTask.SetActive(false);
+            Task2.SetActive(true);
+        }
+        if(col.gameObject != chooseObject)
+        {
+
+            SceneManager.LoadScene(2);
+            soundObject.GetComponent<AudioSource>().Play();
+        }
     }
 }
